eb3eb9990bc0666b26cef1942562a3aa164ca999
GalanLuis <holagalndrew@gmail.com>
2022-03-14 20:21:44 -0600

    Indice segundo sprint y respuesta por defecto, inicio y termino de conversacion

--- chatbot/functions.py
+++ chatbot/functions.py
@@ -1,17 +1,41 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, dispatcher
 
-comandosChatBotGE = 'Para comenzar escribe o elige alguno de los siguientes comandos: \nPagina de Gestion Escolar: /paginaGE'
+chatBotGE_comandos = 'Hola, mucho gusto. Para comenzar escribe o elige alguno de los siguientes comandos relacionados a los temas de las dudas y preguntas más frecuentes de los alumnos hacia el departamento de Gestión Escolar de la UPIICSA:\n\nConstancias: /constancias\nElectivas: /electivas\nReinscripción: /reinscripcion\nEquivalencias y Rev. De materias: /equivalenciasYrevalidacion\nOtros:\nPágina de Gestión Escolar de la UPIICSA: /paginaGE\n\nRecuerda que para escribir un comando este debe de comenzar por una "/" seguido de la palabra clave del tema a consultar sin espacios entre palabras'
+
+constancias_comandos = 'Por favor, elige o escribe el comando relacionado con las dudas y preguntas más frecuentes para las Constancias:\n'
+electivas_comandos = 'Por favor, elige o escribe el comando relacionado con las dudas y preguntas más frecuentes para las Electivas:\n'
+reinscripcion_comandos = 'Por favor, elige o escribe el comando relacionado con las dudas y preguntas más frecuentes para las Reinscripciones:\n'
+erMaterias_comandos = 'Por favor, elige o escribe el comando relacionado con las dudas y preguntas más frecuentes para Equivalencias y Revalidación de materias:\n'
+respuestaPorDefecto_comandos = 'Hola, es posible que el comando que ingresaste no es reconocido debido a un error de sintaxis o posiblemente no esté registrado dentro de las funciones que tiene este ChatBot, te recomendamos intentar nuevamente.\nEn caso de que el problema persista por favor, envía un correo directamente al área de Gestión Escolar de la UPIICSA en donde se te podrá atender de forma personalizada\nCorreo: tramitesgestionescolar@gmail.com o visita la página: https://www.upiicsa.ipn.mx/estudiantes/gestion-escolar.html.\n\nDe igual manera puedes consultar todos los temas que abarca el ChatBot con el siguiente comando: /start'
 
 
+#funciones para establecer y finalizar conversacion con el ChatBot
 def start(update, context):    
-    context.bot.send_message(chat_id=update.effective_chat.id, text=comandosChatBotGE)
-
-def hello(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Encender auto')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=chatBotGE_comandos)
 
 def saludar(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Mucho gusto, en que puedo servirte?')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Mucho gusto, en que puedo servirte?, puedes ver el menú de todos los temas disponibles en este ChatBot tecleando o seleccionando el siguiente comando: /start')
+
+def finalizarConsulta(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Fue un placer atenderte, hasta luego.')
 
 def paginaGe(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='La pagina de gestion escolar es:https://www.upiicsa.ipn.mx/estudiantes/gestion-escolar.html')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='La página de Gestión Escolar de la UPIICSA es:https://www.upiicsa.ipn.mx/estudiantes/gestion-escolar.html')
 
+
+#PROCESOS PARA EL sprint 2
+def constancias(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=constancias_comandos)
+
+def electivas(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=electivas_comandos)
+
+def reinscripcion(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reinscripcion_comandos)
+
+def e_r_materias(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=erMaterias_comandos)
+
+#Funcion para respoder por defecto a todos los comandos no registrados en el ChatBot
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=respuestaPorDefecto_comandos)
--- chatbot/handler.py
+++ chatbot/handler.py
@@ -1,17 +1,40 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, dispatcher
 from constants import BOT_TOKEN
-from functions import saludar, hello, paginaGe, start
+from functions import saludar, paginaGe, start, constancias, electivas, reinscripcion, e_r_materias, unknown, finalizarConsulta
 
 updater = Updater(token=BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 def start_bot():    
+    #Comandos de inicio y fin para establecer comunicacion con el ChatBot
     start_handler = CommandHandler('start', start)
-    hello_handler = CommandHandler('iran', hello)
-    saludar_handler = CommandHandler('hola', saludar)
+    saludar1_handler = CommandHandler('hola', saludar)
+    saludar2_handler = CommandHandler('buenosDias', saludar)
+    saludar3_handler = CommandHandler('buenasTardes', saludar)
+    saludar4_handler = CommandHandler('buenasNoches', saludar)
+    finalizarConsulta1_handler = CommandHandler('gracias', finalizarConsulta)
+    finalizarConsulta2_handler = CommandHandler('muchasGracias', finalizarConsulta)
+    finalizarConsulta3_handler = CommandHandler('adios', finalizarConsulta)
     paginaGe_handler = CommandHandler('paginaGE', paginaGe)
+    #Aqui ya comienzan los procesos para el sprint 2
+    constancias_handler = CommandHandler('constancias', constancias)
+    electivas_handler = CommandHandler('electivas', electivas)
+    reinscripcion_handler = CommandHandler('reinscripcion', reinscripcion)
+    equivalencias_r_handler = CommandHandler('equivalenciasYrevalidacion', e_r_materias)
+    #funcion por defecto para comandos no registrados en el ChatBot
+    unknown_handler = MessageHandler(Filters.command, unknown)
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(hello_handler)
-    dispatcher.add_handler(saludar_handler)
+    dispatcher.add_handler(saludar1_handler)
+    dispatcher.add_handler(saludar2_handler)
+    dispatcher.add_handler(saludar3_handler)
+    dispatcher.add_handler(saludar4_handler)
+    dispatcher.add_handler(finalizarConsulta1_handler)
+    dispatcher.add_handler(finalizarConsulta2_handler)
+    dispatcher.add_handler(finalizarConsulta3_handler)
     dispatcher.add_handler(paginaGe_handler)
+    dispatcher.add_handler(constancias_handler)
+    dispatcher.add_handler(electivas_handler)
+    dispatcher.add_handler(reinscripcion_handler)
+    dispatcher.add_handler(equivalencias_r_handler)
+    dispatcher.add_handler(unknown_handler)
     updater.start_polling()
